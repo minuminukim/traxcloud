f30c7b7ccd7381a680eecba4066bd177894f2f1f
Minu Kim <minumoseskim@gmail.com>
2022-01-06 16:59:34 -0500

    Add sign up validation

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,12 +3,14 @@ const asyncHandler = require('express-async-handler');
 
 const { User } = require('../../db/models');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { validateSignup } = require('../../utils/validation');
 
 const router = express.Router();
 
 // Sign Up
 router.post(
   '/',
+  validateSignup,
   asyncHandler(async (req, res) => {
     const { email, username, password } = req.body;
     const user = await User.signup({ email, username, password });
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -16,6 +16,7 @@ const handleValidationErrors = (req, _res, next) => {
   next();
 };
 
+// Validators
 const validateLogin = [
   check('credential')
     .exists({ checkFalsy: true })
@@ -27,7 +28,25 @@ const validateLogin = [
   handleValidationErrors,
 ];
 
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least four characters.'),
+  check('username').not().isEmail().withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be six characters or more.'),
+  handleValidationErrors,
+];
+
 module.exports = {
   handleValidationErrors,
   validateLogin,
+  validateSignup,
 };
