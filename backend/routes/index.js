841d1b80f5b541884e4acdb03db50b63c9de20de
Minu Kim <minumoseskim@gmail.com>
2022-01-06 17:00:38 -0500

    Clean up hello world test route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,20 +5,4 @@ const router = express.Router();
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', (req, res) => {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
-
-// fetch('/api/test', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'XSRF-TOKEN': `BtiMH600-rQjn8y97U4ck2TCgxD8mziqNxCg`,
-//   },
-//   body: JSON.stringify({ hello: 'world' }),
-// })
-//   .then((res) => res.json())
-//   .then((data) => console.log(data));
-
 module.exports = router;
