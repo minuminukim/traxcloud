7ea3c9de8fac7eab676db3309a2b2137485a5346
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-03-17 04:27:06 -0400

    Build CommentField component

---
+++ frontend/src/components/CommentField/CommentField.js
@@ -0,0 +1,68 @@
+import { useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { postComment } from '../../actions/commentActions';
+import ProfilePicture from '../common/ProfilePicture';
+import InputField from '../common/InputField';
+import './CommentField.css';
+
+const generateRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+const CommentField = ({ duration }) => {
+  const dispatch = useDispatch();
+  const formRef = useRef(null);
+  const { trackId } = useParams();
+  const { user } = useSelector((state) => state.session);
+  const [body, setBody] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('howdy');
+    if (!body.length) return;
+
+    const commentData = {
+      userId: user.id,
+      trackId: +trackId,
+      timePosted: generateRandomInt(duration),
+      body,
+    };
+
+    return dispatch(postComment(commentData))
+      .then((comment) => console.log('comment', comment))
+      .then(() => setBody(''))
+      .catch((error) => console.log('error posting comment', error));
+  };
+
+  const handleKeyUp = (e) => {
+    e.stopPropagation();
+    // Enter key
+    if (e.keyCode === 13) {
+      formRef.current.click();
+    }
+  };
+
+  const handleInputChange = (e) => setBody(e.target.value);
+
+  return (
+    <form
+      className="comment-field"
+      ref={formRef}
+      onSubmit={handleSubmit}
+      // onKeyUp={handleKeyUp}
+    >
+      <ProfilePicture user={user} size="medium" shape="square" />
+      <InputField
+        id="comment"
+        placeholder="Write a comment"
+        value={body}
+        onChange={handleInputChange}
+        onKeyUp={handleKeyUp}
+      />
+      <input type="submit" hidden ref={formRef} />
+    </form>
+  );
+};
+
+export default CommentField;
