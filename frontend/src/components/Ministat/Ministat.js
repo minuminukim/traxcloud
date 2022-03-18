0210d384fd4a1e6a082da7f317a0d6c791984f19
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-03-18 16:35:45 -0400

    Ministat styles

--- frontend/src/components/Ministat/Ministat.js
+++ frontend/src/components/Ministat/Ministat.js
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import { FaCommentAlt } from 'react-icons/fa';
+import './Ministat.css';
 
 const Ministat = ({ type, count, trackId }) => {
   if (type === 'comment') {
     return (
-      <Link to={`/tracks/${trackId}`}>
-        <FaCommentAlt />
-        {count}
+      <Link className="ministat" to={`/tracks/${trackId}`}>
+        <FaCommentAlt className="ministat-icon" />
+        <span className="ministat-count">{count}</span>
       </Link>
     );
   }
