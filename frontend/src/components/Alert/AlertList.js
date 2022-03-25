60dde0f45ce6d172441160b6ba70ab52e5d043d0
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-03-25 14:49:32 -0400

    Adjust player size and add alert popups on success

--- frontend/src/components/Alert/AlertList.js
+++ frontend/src/components/Alert/AlertList.js
@@ -1,12 +1,12 @@
 import Alert from './Alert';
 import './Alert.css';
 
-const AlertList = ({ messages }) => {
+const AlertList = ({ messages, isError = true }) => {
   return (
     messages.length > 0 && (
       <ul className="alert-list">
         {messages.map((message) => (
-          <Alert key={message} message={message} />
+          <Alert key={message} message={message} isError={isError} />
         ))}
       </ul>
     )
